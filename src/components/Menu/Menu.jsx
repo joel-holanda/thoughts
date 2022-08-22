22f18dcd28c4be9fb216771bd0e1bc6c5f8fadfa
Joel Holanda <joelholanda45@gmail.com>
2022-08-22 20:26:44 -0300

    ajeitado com sucesso

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,6 +1,6 @@
 import '../Menu/Menu.css'
 
-function Menu(){
+function Menu(props){
     var text = "Leia e Reflita :)"
     return(
     <>
@@ -8,7 +8,7 @@ function Menu(){
             <div className='title'>
                 <h1>{text}</h1>
             </div>
-            <a href="https://thoughts-ivory.vercel.app/create"><img src="https://img.icons8.com/ios-glyphs/452/pencil--v1.png" className='pencil'/></a>
+            <a href={props.link}><img src="https://img.icons8.com/ios-glyphs/452/pencil--v1.png" className='pencil'/></a>
             {/* <img src='https://boletando.com/wp-content/uploads/avatars/1355/avatar-bpfull.jpg' className='avatar'/> */}
         </div>
     </>
