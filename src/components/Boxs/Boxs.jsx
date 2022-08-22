bbca32e8a530f5af9fb66ae274d67b8ed2d05468
Joel Holanda <joelholanda45@gmail.com>
2022-08-22 19:46:48 -0300

    integracao com enviar dados

--- src/components/Boxs/Boxs.jsx
+++ src/components/Boxs/Boxs.jsx
@@ -1,12 +1,58 @@
 import '../Boxs/Boxs.css'
+import { useState, useEffect } from "react" 
+import { getFirestore, collection, getDocs, addDoc} from "firebase/firestore"
+import { db } from '../../firebase/firebase' 
+
+
+
 
 function Boxs(){
+    const [ title, setTitle] = useState("");
+    const [ text, setText ] = useState("");
+    const [ author, setAuthor] = useState("");
+    const [ users, setUsers] = useState([]);
+    
+    const usersColletionRef = collection(db, "users")
+
+    async function criarDado() {
+        try {
+          const user = await addDoc(collection(db, "users"), {
+            title,
+            text,
+            author,
+          });
+
+          alert("enviado com sucesso :)")    
+          console.log("dados salvos com sucessos", user);
+
+        } catch (e) {
+          console.error("Error adding document: ", e);
+        }
+      }
+
     return(
         <div className='container-box'>
-            <input type="text" placeholder="Titulo (não é obrigatorio)" className='box-title'/>
-            <input type="text" placeholder="Seu pensamento..." className='box-text'/>
-            <input type="text" placeholder="Autor (não é obrigatorio)" className='box-author'/>
-            <button className='submit'>Enviar</button>
+            <input 
+            type="text"
+            placeholder="Titulo (não é obrigatorio)" className='box-title'
+            value={title} 
+            onChange={(e) => setTitle(e.target.value)}/>
+
+            <input 
+            type="text" 
+            placeholder="Seu pensamento..." 
+            className='box-text'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            />
+
+            <input 
+            type="text"
+            placeholder="Autor (não é obrigatorio)" className='box-author'
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}/>
+
+            <button className='submit' onClick={criarDado}>Enviar</button>
         </div>
     )
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -9,7 +9,7 @@ function Menu(){
                 <h1>{text}</h1>
             </div>
             <a href="http://localhost:5173/create"><img src="https://img.icons8.com/ios-glyphs/452/pencil--v1.png" className='pencil'/></a>
-            <img src='https://boletando.com/wp-content/uploads/avatars/1355/avatar-bpfull.jpg' className='avatar'/>
+            {/* <img src='https://boletando.com/wp-content/uploads/avatars/1355/avatar-bpfull.jpg' className='avatar'/> */}
         </div>
     </>
 )
