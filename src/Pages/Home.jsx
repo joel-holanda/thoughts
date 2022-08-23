021cdbb8f1e53150f47669928c853dd755f27183
Joel Holanda <joelholanda45@gmail.com>
2022-08-23 13:35:30 -0300

    mudando link para uma imagem

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Texts from '../components/Texts/Texts'
 function Home(){
     return(
         <>
-            <Menu link="/create"/>
+            <Menu link="https://imagens.usp.br/wp-content/uploads/Campus-15-Foto-Marcos-Santos20101220_066.jpg"/>
             <Texts />
         </>
     )
